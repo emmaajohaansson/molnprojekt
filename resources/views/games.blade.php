5fcb7de3c97a2fd907a4e6b771f81076fd377f3c
Philipdev95 <Philipdev95gmail.com>
2018-05-28 14:08:45 +0200

    fixed styling of all games view, games.blade

--- resources/views/games.blade.php
+++ resources/views/games.blade.php
@@ -8,19 +8,18 @@
 
 
 <div class="row p-3">
-  <div class="card-columns">
-      <?php foreach ($result as &$game): ?>
-    <div class="card text-white bg-dark">
-      <img class="card-img-top" src="https://www.hiphomeschoolmoms.com/wp-content/uploads/2011/07/250x250-sample-215x161.png" alt="Game image">
-      <div class="card-body">
-        <a href=<?php echo "/games/",  $game->id; ?>><h5 class="card-title"><?php echo $game->name; ?></h5></a>
-        <p class="card-text"><?php echo $game->description; ?></p>
-        <p class="card-text"><small class="text-muted">Released at: <?php echo $game->createdAt; ?></small></p>
+    <?php foreach ($result as &$game): ?>
+    <div class="col-4 my-3">
+      <div class="card text-white bg-dark">
+        <img class="card-img-top" src="https://www.hiphomeschoolmoms.com/wp-content/uploads/2011/07/250x250-sample-215x161.png" alt="Game image">
+        <div class="card-body">
+          <a href=<?php echo "/games/",  $game->id; ?>><h5 class="card-title"><?php echo $game->name; ?></h5></a>
+          <p class="card-text"><?php echo $game->description; ?></p>
+          <p class="card-text"><small class="text-muted">Released at: <?php echo $game->createdAt; ?></small></p>
+        </div>
       </div>
     </div>
   <?php endforeach; ?>
-
-  </div>
 </div>
 
 @endsection
