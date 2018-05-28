97e307e2ebee4afb1aebeec82d02d7a02413c6a7
Philipdev95 <philipdev95@gmail.com>
2018-05-28 15:04:35 +0200

    Adding favicon and some css trys

--- resources/views/start.blade.php
+++ resources/views/start.blade.php
@@ -8,11 +8,12 @@
 <div class="row">
   <div class="jumbotron text-center bg-dark text-light rounded-0 width-100">
     <h1 class="display-4 font-weight-light">GAMELINKS.SE</h1>
+    <img src="/img/icon.png" class="width-50" />
     <p class="lead">This site is for made for keeping track of your games and what game to buy or try next. You can see other people reviews and save your games and write your own reviews.</p>
     <hr class="my-4">
-    <a class="btn btn-primary btn-lg" href="/games" role="button">Games</a>
-    <a class="btn btn-primary btn-lg" href="/api/users" role="button">User</a>
-    <a class="btn btn-primary btn-lg" href="/api/reviews" role="button">Reviews</a>
+    <a class="btn3d btn btn-primary btn-lg" href="/games" role="button">Games</a>
+    <a class="btn3d btn btn-primary btn-lg" href="/api/users" role="button">User</a>
+    <a class="btn3d btn btn-primary btn-lg" href="/api/reviews" role="button">Reviews</a>
   </div>
 </div>
 @endsection
