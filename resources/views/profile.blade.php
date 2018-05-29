f620799aa3e421180850758c9b8a968b18d316ae
Philipdev95 <philipdev95@gmail.com>
2018-05-29 12:02:21 +0200

    profile blade

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -5,6 +5,7 @@
 @section('title', 'Page Title')
 
 @section('content')
+
 <div class="row">
   <div class="col-12 col-md-6 text-center p-3">
     <h4 class="text-uppercase font-weight-superlight">Profile</h4>
@@ -32,60 +33,99 @@
       </div>
       <div class="form-control d-flex justify-content-between align-items-center col-8">
         <small>If you really want to, then you can</small>
-        <a class="btn btn-dark btn-sm trans-size-80" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+        <a class="btn btn-dark btn-sm trans-size-80" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('deleteProfile-form').submit();">
           {{ __('Delete Account') }}</a>
       </div>
     </div>
-    <a class="btn btn-danger m-2" href="{{ route('logout') }}"
-       onclick="event.preventDefault();
-                     document.getElementById('logout-form').submit();">
+    <a class="btn btn-sm btn-danger m-2" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
         {{ __('Logout') }}
     </a>
-    <a class="btn btn-info m-2" href="{{ route('logout') }}"
+    <a class="btn btn-sm btn-info m-2" href="{{ route('logout') }}"
        onclick="event.preventDefault();
-                     document.getElementById('logout-form').submit();">
+                     document.getElementById('updateProfile-form').submit();">
         {{ __('Update') }}
     </a>
   </div>
   <!-- SPLIT -->
   <!-- SPLIT -->
   <!-- SPLIT -->
-  <div class="card col-12 col-md-6 text-center my-3">
-    <div class="card-body">
+  <div class="col-12 col-md-6 text-center my-3">
+    <div class="py-2">
       <h4 class="text-uppercase font-weight-superlight">
-          Created Games
+        Add Game
       </h4>
-      <ul class="list-group list-group-flush text-left">
-        <li class="list-group-item d-flex justify-content-between align-items-center">
-          Game One
-          <a href="#deleteGame" class="btn btn-danger btn-lg">
+      <form>
+        <div class="form-group input-group mb-3 text-left">
+          <div class="input-group-prepend">
+            <span class="input-group-text" id="gameTitle">Game Title</span>
+          </div>
+          <input type="title" class="form-control form-control-sm" id="basic-url" aria-describedby="nameOfGame">
+        </div>
+        <div class="form-group input-group text-left">
+          <div class="input-group-prepend">
+            <span class="input-group-text">Game <br />Description</span>
+          </div>
+          <textarea type="text" class="form-control form-control-sm" aria-label="With textarea" id="gameDescription" rows="3"></textarea>
+        </div>
+        <div class="form-check mb-3">
+          <input type="checkbox" class="form-check-input" id="iOwnThis">
+          <label class="form-check-label" for="iOwnThis">I own this game</label>
+        </div>
+        <button type="submit" class="btn btn-sm btn-primary">Add game</button>
+      </form>
+    </div>
+  </div>
+</div>
+<div class="row">
+  <div class="py-3 col-12">
+    <h4 class="text-uppercase text-center font-weight-superlight">
+        Created Games
+    </h4>
+    <ul class="list-group list-group-flush text-left">
+      <li class="list-group-item d-flex justify-content-between align-items-center">
+        <p class="col-3">Game One</p>
+        <p class="col-7 font-size-50 text-left overflow-hide">
+          Game Description Game Description Game Description Game Description Game Description Game Description
+          Game Description Game Description Game Description Game Description Game Description Game Description</p>
+        <div class="col-2">
+          <a href="#" class="btn btn-primary btn" data-toggle="modal" data-target="#editGameModal">
+            <i class="fal fa-sync"></i>
+          </a>
+          <a href="#" class="btn btn-danger btn">
             <i class="fal fa-trash-alt"></i>
           </a>
-        </li>
-      </ul>
-      <div class="py-5">
-        <h4 class="text-uppercase font-weight-superlight">Add Game</h4>
-        <form>
-          <div class="form-group input-group mb-3 text-left">
-            <div class="input-group-prepend">
-              <span class="input-group-text" id="gameTitle">Game Title</span>
+        </div>
+      </li>
+    </ul>
+  </div>
+  <div class="modal fade" id="editGameModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Edit Game</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <form>
+            <div class="form-group">
+              <label for="gameTitleUpdate" class="col-form-label">Game Title:</label>
+              <input value="{gameTitle}" type="text" class="form-control" id="gameTitleUpdate">
             </div>
-            <input type="title" class="form-control form-control-sm" id="basic-url" aria-describedby="nameOfGame">
-          </div>
-          <div class="form-group input-group text-left">
-            <div class="input-group-prepend">
-              <span class="input-group-text">Game <br />Description</span>
+            <div class="form-group">
+              <label for="gameDescriptionUpdate" class="col-form-label">Game Description:</label>
+              <textarea class="form-control" id="gameDescriptionUpdate">{gameDescription}</textarea>
             </div>
-            <textarea type="text" class="form-control form-control-sm" aria-label="With textarea" id="gameDescription" rows="3"></textarea>
-          </div>
-          <div class="form-check mb-3">
-            <input type="checkbox" class="form-check-input" id="iOwnThis">
-            <label class="form-check-label" for="iOwnThis">I own this game</label>
-          </div>
-          <button type="submit" class="btn btn-sm btn-primary">Add game</button>
-        </form>
+          </form>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-primary">Save</button>
+        </div>
       </div>
     </div>
   </div>
 </div>
+
 @endsection
