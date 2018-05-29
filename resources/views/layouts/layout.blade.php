9d66d00e69ff0712e38c4742e32ad6a9dd243975
Philipdev95 <philipdev95@gmail.com>
2018-05-29 11:07:05 +0200

    Profile page and account menu

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -52,21 +52,23 @@
                 <li><a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a></li>
                 <li><a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a></li>
             @else
-                <li class="nav-item dropdown">
+                <li class="nav-item dropdown text-center">
                     <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                         {{ Auth::user()->username }} <span class="caret"></span>
                     </a>
 
                     <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="{{ route('logout') }}"
-                           onclick="event.preventDefault();
-                                         document.getElementById('logout-form').submit();">
-                            {{ __('Logout') }}
-                        </a>
+                      <h6 class="dropdown-header">Account menu</h6>
+                      <a class="dropdown-item" href="profile">
+                        Profile
+                      </a>
+                      <a class="dropdown-item text-danger" href="{{ route('logout') }}" onclick="event.preventDefault();document.getElementById('logout-form').submit();">
+                        {{ __('Logout') }}
+                      </a>
 
-                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                            @csrf
-                        </form>
+                      <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                          @csrf
+                      </form>
                     </div>
                 </li>
             @endguest
--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -6,29 +6,50 @@
 
 @section('content')
 <div class="row">
-  <div class="card col-6 text-center my-3">
-    <div class="card-body">
-      <h4 class="display-4 font-size-200 text-uppercase font-weight-superlight">
-          {{ Auth::user()->username }}
-      </h4>
-      <ul class="list-group list-group-flush text-left">
-        <li class="list-group-item">Your id: {{ Auth::user()->id }}</li>
-        <li class="list-group-item">Amount of games: {{ Auth::user()->id }}</li>
-        <li class="list-group-item">Amount of reviews: {{ Auth::user()->id }}</li>
-        <li class="list-group-item d-flex justify-content-between align-items-center">
-          If you really want to, then you can
-          <a class="btn btn-dark btn-sm" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-              {{ __('Delete Account') }}</a>
-        </li>
-      </ul>
+  <div class="col-12 col-md-6 text-center p-3">
+      <div class="input-group input-group-sm my-3">
+        <div class="input-group-prepend col-4">
+          <span class="input-group-text col-12" id="username">Username</span>
+        </div>
+        <input class="form-control text-uppercase font-weight-superlight col-8" value="{{ Auth::user()->username }}">
+      </div>
+      <div class="input-group input-group-sm mb-3">
+        <div class="input-group-prepend col-4 ml-0">
+          <span class="input-group-text col-12" id="password">Password</span>
+        </div>
+        <input class="form-control text-uppercase font-weight-superlight" value="New Password">
+      </div>
+      <div class="input-group input-group-sm mb-3">
+        <div class="input-group-prepend col-4">
+          <span class="input-group-text col-12" id="userId">User Id</span>
+        </div>
+        <input class="form-control text-center col-8" type="text" placeholder="{{ Auth::user()->id }}" readonly>
+      </div>
+      <div class="input-group input-group-sm mb-3">
+        <div class="input-group-prepend col-4">
+          <span class="input-group-text col-12" id="deleteUser">Delete <br />account</span>
+        </div>
+        <div class="form-control d-flex justify-content-between align-items-center col-8">
+          <small>If you really want to, then you can</small>
+          <a class="btn btn-dark btn-sm trans-size-80" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+            {{ __('Delete Account') }}</a>
+        </div>
+      </div>
       <a class="btn btn-danger m-2" href="{{ route('logout') }}"
          onclick="event.preventDefault();
                        document.getElementById('logout-form').submit();">
           {{ __('Logout') }}
       </a>
-    </div>
+      <a class="btn btn-info m-2" href="{{ route('logout') }}"
+         onclick="event.preventDefault();
+                       document.getElementById('logout-form').submit();">
+          {{ __('Update') }}
+      </a>
   </div>
-  <div class="card col-6 text-center my-3">
+  <!-- SPLIT -->
+  <!-- SPLIT -->
+  <!-- SPLIT -->
+  <div class="card col-12 col-md-6 text-center my-3">
     <div class="card-body">
       <h4 class="display-4 font-size-200 text-uppercase font-weight-superlight">
           Dina spel
@@ -44,15 +65,19 @@
       <div class="py-5">
         <h4 class="font-weight-superlight">Add Game</h4>
         <form>
-          <div class="form-group text-left">
-            <label for="gameTitle">Game Title</label>
-            <input type="title" class="form-control form-control-sm" id="gameTitle" aria-describedby="nameofgame" placeholder="Game title">
+          <div class="form-group input-group mb-3 text-left">
+            <div class="input-group-prepend">
+              <span class="input-group-text" id="gameTitle">Game Title</span>
+            </div>
+            <input type="title" class="form-control form-control-sm" id="basic-url" aria-describedby="nameOfGame">
           </div>
-          <div class="form-group text-left">
-            <label for="gameDescription">Description</label>
-            <input type="text" class="form-control form-control-sm" id="gameDescription" placeholder="Game Description">
+          <div class="form-group input-group text-left">
+            <div class="input-group-prepend">
+              <span class="input-group-text">Game <br />Description</span>
+            </div>
+            <textarea type="text" class="form-control form-control-sm" aria-label="With textarea" id="gameDescription" rows="3"></textarea>
           </div>
-          <div class="form-check text-left">
+          <div class="form-check mb-3">
             <input type="checkbox" class="form-check-input" id="iOwnThis">
             <label class="form-check-label" for="iOwnThis">I own this game</label>
           </div>
