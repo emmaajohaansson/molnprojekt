688a465a16e71726a9ede414a01be08c45f27812
Philipdev95 <philipdev95@gmail.com>
2018-05-30 17:49:36 +0200

    start blade

--- resources/views/start.blade.php
+++ resources/views/start.blade.php
@@ -5,8 +5,8 @@
 @section('title', 'Start')
 
 @section('content')
-<div class="row height-100 subtract-header">
-  <div class="bg-dark m-0 p-0 text-white text-center text-light width-100">
+<div class="row height-100 subtract-header pt-4">
+  <div class="bg-dark m-0 pt-4 text-white text-center text-light width-100">
     <!--img class="m-0 p-0 rounded-0 card-img" style="filter: blur(5px) brightness(0.70)" src="https://i.pinimg.com/originals/2a/83/03/2a8303314ac11ecadf65d6e0d0f2a1aa.jpg" alt="Card image"-->
     <div class="bg-black-50 p-4 bg-cover-center bg-image-1 height-100 font-weight-superlight">
       <h1 class="display-2 font-weight-superlight">GAMELINK.SE</h1>
