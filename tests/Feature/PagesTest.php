1a46607e6c52575a108896fabf721564ae6db3c3
Oscar Andersson <anderssonoscar0@gmail.com>
2018-05-31 11:42:25 +0200

    DEPLOY!

--- tests/Feature/PagesTest.php
+++ tests/Feature/PagesTest.php
@@ -12,20 +12,21 @@ class ExampleTest extends TestCase
     /* @test */
     public function test_home_page()
     {
-        
+
         $this->visit("/")
             ->see("GAMELINK.SE")
             ->dontSee("somethingelse");
     }
 
+
     public function test_games_page()
     {
-        
+
         $this->visit("/games")
             ->see("No games")
             ->dontSee("Super mario!");
     }
 
-    
+
 
 }
