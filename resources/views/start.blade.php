b9390dfa48a56d7c56bc2e9f4781ef42a6748004
Oscar Andersson <anderssonoscar0@gmail.com>
2018-06-01 21:26:32 +0200

    DEPLOY WORKS WOHOHOOHOHOHO

--- resources/views/start.blade.php
+++ resources/views/start.blade.php
@@ -9,7 +9,7 @@
   <div class="bg-cover-center bg-image-1 m-0 pt-4 text-white text-center text-light height-100 width-100">
     <!--img class="m-0 p-0 rounded-0 card-img" style="filter: blur(5px) brightness(0.70)" src="https://i.pinimg.com/originals/2a/83/03/2a8303314ac11ecadf65d6e0d0f2a1aa.jpg" alt="Card image"-->
     <div class="bg-black-50 p-4 height-100 font-weight-superlight">
-      <h1 class="display-4 font-weight-superlight">GAMELINK</h1>
+      <h1 class="display-4 font-weight-superlight">GAMELINKISFREAKINGAMAZING!!</h1>
       <h4 class="display-4 font-weight-superlight"><i class="fal fa-gamepad"></i> + <i class="fal fa-link"></i></h4>
       <p class="card-text p-3 font-weight-light">This site is for made for keeping track of your games and what game to buy or try next.
         <br />You can see other people reviews and save your games and write your own reviews.
