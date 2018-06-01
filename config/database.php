72ed9c5ade1daa0923775b2c0665404e82ffafe8
Oscar Andersson <anderssonoscar0@gmail.com>
2018-06-01 21:21:01 +0200

    s

--- config/database.php
+++ config/database.php
@@ -35,11 +35,6 @@ return [
     
     'connections' => [
 
-        'sqlite' => [
-            'driver' => 'sqlite',
-            'database' => env('DB_DATABASE', database_path('database.sqlite')),
-            'prefix' => '',
-        ],
 
         'mysql' => [
             'driver' => 'mysql',
